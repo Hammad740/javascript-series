68976bc42f52bbf786ce277bb102917af7139d89
Mohammad Hammad Ansari <mohammadhammadansari07@gmail.com>
2023-09-25 13:45:40 +0000

    Basics of maths & numbers

---
+++ 01_basics/08_Nums&Maths.js
@@ -0,0 +1,41 @@
+const score=400;
+console.log(score);
+
+const balance=new Number(100)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(3));
+const hundreds=100000
+console.log(hundreds.toLocaleString());//US style
+console.log(hundreds.toLocaleString('en-IN'));//indian style
+
+
+//+++++++++Maths++++++++
+// go into console for more methods
+console.log(Math);
+console.log(Math.abs(-34));
+console.log(Math.round(465.753));
+console.log(Math.ceil(235.45)); //takes upper value 
+console.log(Math.floor(235.45));//takes lower value
+console.log(Math.min(3,45,4523,4626,7575));
+console.log(Math.max(3,45,4523,4626,7575));
+
+console.log(Math.random());//value b/w (0-1)
+console.log(Math.random()*10); 
+console.log(Math.random()*100); 
+console.log((Math.random()*10)+1); 
+console.log((Math.random()*10)+1); 
+console.log(Math.floor((Math.random()*10))+1); //1-9
+
+
+const min=10
+const max=20
+
+
+
+console.log(Math.floor(Math.random()*(max-min +1))+min)
+
+
+
+
+
