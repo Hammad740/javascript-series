7e2277b573cc6ae061f61853727a2ad3315cf9a3
Mohammad Hammad Ansari <mohammadhammadansari07@gmail.com>
2023-09-25 10:04:15 +0000

    conversion of datatypes

---
+++ 01_basics/typeconversion.js
@@ -0,0 +1,15 @@
+let score="105abc";
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+let someNumber=100;
+console.log(String(someNumber));
+console.log(typeof(String(someNumber)))
+//"33"=>33
+//"33avc"=>NaN
+// true=>1
+// false=>0
