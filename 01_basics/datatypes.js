a9e1c757bce25706b4456b2c8f5fd38c5730597e
Mohammad Hammad Ansari <mohammadhammadansari07@gmail.com>
2023-09-25 09:48:58 +0000

    learned datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; /// treat all js code as newer version
+
+//alert("hello"); // we are using nodejs,not browser
+
+//number=>2^53
+//bigint
+//string=>""
+//boolean
+//null=>standalone value (null means empty)
+//undefined=>
+//symbol=>unique
+
+//object
+
+console.log(typeof(34));
+console.log(typeof undefined);
+console.log(typeof null);
