4fd7980c0707e7ef8c8c91f448b2200a3f4e576b
Mohammad Hammad Ansari <mohammadhammadansari07@gmail.com>
2023-09-25 10:24:33 +0000

    Arithmetic operations

---
+++ 01_basics/operation.js
@@ -0,0 +1,27 @@
+let value=3;
+let negValue=-value
+console.log(value,negValue);
+console.table([value*value,value-value,value/value,value**value,value%value]);
+
+//tricky part
+let str1="hello"
+let str2="hammad"
+let str3=str1 + str2
+console.log(str3);
+
+console.log("1"+3);
+console.log("1"+"3");
+console.log(1+"3"+2);
+console.log(2+3+"1"+3);
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3;
+num1=num2=num3=56
+console.log(num1,num2,num3)
+
+let counter=100
+counter++;
+console.log(counter);
+
