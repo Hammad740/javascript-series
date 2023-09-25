8cc20852d6a82b107e0a964817e1b678421863aa
Mohammad Hammad Ansari <mohammadhammadansari07@gmail.com>
2023-09-25 06:32:11 +0000

    understand variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId=145553
+let accountEmail="hammad@gamil.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState; /// udefined will be printed
+
+
+//accountId=2  const cannot be changed
+accountEmail="hammad208@gmail.com"
+accountPassword="90874";
+accountCity="Banglore";
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
+/*prefer not to use var because of issue of functional and block scope */
