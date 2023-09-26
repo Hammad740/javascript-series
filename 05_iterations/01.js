9ddc82d54088689a9bc8876726b38dad59efc2ff
Mohammad Hammad Ansari <mohammadhammadansari07@gmail.com>
2023-09-26 14:06:17 +0000

    loops

---
+++ 05_iterations/01.js
@@ -0,0 +1,18 @@
+// loops
+ const array=[1,2,3,4,5,6,7,8,9,10]
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element);
+}
+
+let x=5;
+while (x--) {
+ console.log("i am a happy man until i started engineering");   
+}
+let y=5
+do {
+    let score =10;
+    console.log(`Score is ${score}`);
+    score=score+5
+} while (y--);
+
